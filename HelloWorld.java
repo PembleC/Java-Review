60b427f690811c2609483adb2054e6542da2ab2f
Carson P <PembleC@oregonstate.edu>
2020-02-28 16:57:17 -0800

    Basic HelloWorld
    I also included some variable types and comments

---
+++ HelloWorld.java
@@ -0,0 +1,24 @@
+// This is a Java comment
+
+/* This is a big comment and
+can span...
+multiple...
+lines */
+
+
+class HelloWorld {
+  public static void main(String[] args) {
+    boolean b;
+    char c;
+    int i;
+    short s;
+    byte by;
+    long l;
+    float f;
+    double d;
+
+    String str;
+
+    System.out.println("Hello, world!");
+  }
+}
